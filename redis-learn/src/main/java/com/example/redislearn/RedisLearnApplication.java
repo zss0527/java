2bc44f7102dc3833b560a9b97c9ebaf7944d56da
zhushuaishuai <zss0527@outlook.com>
2025-03-02 18:38:37 +0800

    update

---
+++ redis-learn/src/main/java/com/example/redislearn/RedisLearnApplication.java
@@ -0,0 +1,14 @@
+package com.example.redislearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLearnApplication.class, args);
+        System.out.println("Hello World~");
+    }
+
+}
---
+++ redis-learn/src/test/java/com/example/redislearn/RedisLearnApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.redislearn;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
+
+@SpringBootTest
+class RedisLearnApplicationTests {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
